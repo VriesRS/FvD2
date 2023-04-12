28180c052a7bf61899dbc55697891ffea64819e7
VriesRS <112829709+VriesRS@users.noreply.github.com>
2023-04-12 11:40:38 +0200

    Add files via upload

--- opdracht2/script.js
+++ opdracht2/script.js
@@ -1,14 +1,4 @@
-document.querySelector('li').addEventListener('click', selected);
-
-
-function selected() {
-    let books = document.querySelector('li');
-    books.classList.toggle('selected');
-    console.log("test1")
-
-}
-
-
+// de vall animatie starten nadat de eerste animatie is afgelopen
 
 const film = document.querySelector('li');
 
@@ -18,128 +8,47 @@ film.addEventListener('animationend', () => {
 });
 
 
-// row1.addEventListener('keypress', function(e) {
-//     if (e.target != this){
-//         row2.appendChild(e.target.cloneNode(true));
-//         e.target.remove();
-//     } 
-// })
-
-
-// row2.addEventListener('keypress', function(e) {
-//     if (e.target != this){
-//         selected.appendChild(e.target.cloneNode(true));
-//         e.target.remove();
-//     } 
-// })
-
-
-
-// $('body').on('click', 'li', function() {
-//     $(this).toggleClass('selected');
-//  });
-//  $('#move_left').click(function() {
-//      $('.row1').append($('.row2 .selected').removeClass('selected'));
-//  })
-//  $('#move_right').click(function() {
-//      $('.row2').prepend($('.row1 .selected').removeClass('selected'));
-//  })
-
-
-
-
-//  let selectedItem = null;
-
-//  function selectItem(item) {
-//    if (selectedItem) {
-//      selectedItem.classList.remove('selected');
-//    }
-//    selectedItem = item;
-//    selectedItem.classList.add('selected');
-//  }
-
-//  function moveLeft() {
-//    let list2 = document.getElementById("list2");
-//    let selectedItems = list2.querySelectorAll("li.selected");
-//    for (let i = 0; i < selectedItems.length; i++) {
-//      document.getElementById("list1").appendChild(selectedItems[i]);
-//      selectedItems[i].classList.remove('selected');
-//    }
-//  }
-
-//  function moveRight() {
-//    let list1 = document.getElementById("list1");
-//    let selectedItems = list1.querySelectorAll("li.selected");
-//    for (let i = 0; i < selectedItems.length; i++) {
-//      document.getElementById("list2").appendChild(selectedItems[i]);
-//      selectedItems[i].classList.remove('selected');
-//    }
-//  }
-// </script>
-// </body>
-// </html>
-// This code defines two unordered lists with items that can be selected by clicking on them. When an item is clicked, it becomes the selected item and its background color changes to indicate that it is selected. The code also defines two buttons that allow you to move selected items from one list to the other. When a button is clicked, the code finds all selected items in the current list, moves them to the other list, and removes their "selected" class.
-
-
-
-
-
-
-
-
-// FLIP Techniek
-
-
-function duplicateItem() {
-    // Get the first list and the selected item
-    const list1 = document.getElementById('list1');
-    const selected = list1.querySelector('.selected');
   
-    // If no item is selected, return
-    if (!selected) {
-      return;
+// De film uit de eerste lijst over zetten naar de 2de lijst
+
+let list1 = document.getElementById("list1");
+let list2 = document.getElementById("list2");
+
+list1.addEventListener("click", function(event) {
+  let item = event.target;
+  if (item.tagName === "LI") {
+    if (item.classList.contains('selected')) {
+      let newItem = document.createElement("li");
+      newItem.innerHTML = item.innerHTML;
+      list2.appendChild(newItem);
+      item.parentNode.removeChild(item);
+        const popup = document.querySelector('.popup');
+        popup.classList.add('active');
+    } else {
+      item.classList.add('selected');
     }
-  
-    // Clone the selected item
-    const cloned = selected.cloneNode(true);
-  
-    // Add the cloned item to the second list
-    const list2 = document.getElementById('list2');
-    list2.appendChild(cloned);
   }
-  
-  // Add event listeners to select/deselect items
-  const list1 = document.getElementById('list1');
-  list1.addEventListener('click', event => {
-    // Deselect previously selected item
-    const selected = list1.querySelector('.selected');
-    if (selected) {
-      selected.classList.remove('selected');
-    }
-  
-    // Select the clicked item
-    if (event.target.nodeName === 'LI') {
-      event.target.classList.add('selected');
-    }
+});
+
+// de class van de eerste animatie ook voor de 2de lijst activeren
+
+let list2Items = document.querySelectorAll("#list2 li");
+list2Items.forEach((item) => {
+  item.addEventListener("click", function() {
+    item.classList.toggle("selected");
   });
+});
 
 
+// Als er iets uit de 2de lijst geselecteerd is deze met enter kunnen verwijderen
 
-  function duplicateItem() {
-    // Get the first list and the selected item
-    const list1 = document.getElementById('list1');
-    const selected = list1.querySelector('.selected');
-  
-    // If no item is selected, return
-    if (!selected) {
-      return;
+document.addEventListener("keypress", function(event) {
+    if (event.code === "Enter") {
+      let selectedItem = document.querySelector(".selected");
+      if (selectedItem) {
+        selectedItem.remove();
+      }
     }
-  
-    // Clone the selected item
-    const cloned = selected.cloneNode(true);
-  
-    // Add the cloned item to the second list
-    const list2 = document.getElementById('list2');
-    list2.appendChild(cloned);
-  }
-  
+});
+
+
